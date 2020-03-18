40c2a77e74ac4f0eea27e6d2115b325b544e58f5
Charlie <173063877@qq.com>
2020-03-19 07:10:48 +0800

    2020/3/18 公司作业内容

--- app/src/main/java/com/young/timber/adapters/AlbumAdapter.java
+++ app/src/main/java/com/young/timber/adapters/AlbumAdapter.java
@@ -2,9 +2,13 @@ package com.young.timber.adapters;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.young.timber.R;
 
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ItemHolder>{
 
@@ -29,8 +33,17 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ItemHolder>{
 
     public class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        protected TextView title,artist;
+        protected ImageView albumArt;
+        protected View footer;
+
         public ItemHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.album_title);
+            artist = itemView.findViewById(R.id.album_artist);
+            albumArt = itemView.findViewById(R.id.album_art);
+            footer = itemView.findViewById(R.id.footer);
+            itemView.setOnClickListener(this);
         }
 
         @Override
--- app/src/main/java/com/young/timber/utils/NavigationUtils.java
+++ app/src/main/java/com/young/timber/utils/NavigationUtils.java
@@ -3,11 +3,25 @@ package com.young.timber.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Pair;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.young.timber.R;
 import com.young.timber.activities.MainActivity;
 import com.young.timber.activities.NowPlayingActivity;
 
 public class NavigationUtils {
+
+    public static void navigateToAlbum(Activity context, long albumId, Pair<View, String> transitionViews) {
+        FragmentTransaction transaction = ((AppCompatActivity) context).getSupportFragmentManager().beginTransaction();
+        Fragment fragment;
+        transaction.setCustomAnimations(R.anim.activity_fade_in, R.anim.activity_fade_out, R.anim.activity_fade_in, R.anim.activity_fade_out);
+
+    }
 
     public static void navigateToNowplaying(Activity context, boolean withAnimations) {
         Intent intent = new Intent(context, NowPlayingActivity.class);
