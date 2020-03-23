38f6677b7560af287b6873516eda32d2c2da0b94
wandereryoungg <173063877@qq.com>
2020-03-23 23:03:10 +0800

    3/23 在家

--- app/src/main/java/com/young/timber/dataloaders/AlbumLoader.java
+++ app/src/main/java/com/young/timber/dataloaders/AlbumLoader.java
@@ -7,8 +7,28 @@ import android.provider.MediaStore;
 import com.young.timber.models.Album;
 import com.young.timber.utils.PreferencesUtility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumLoader {
 
+    public static List<Album> getAllAlbums(Context context) {
+        return getAlbumsForCursor(makeAlbumCursor(context, null, null));
+    }
+
+    public static List<Album> getAlbumsForCursor(Cursor cursor) {
+        List<Album> albums = new ArrayList<>();
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                albums.add(new Album(cursor.getLong(0), cursor.getString(1), cursor.getString(2),
+                        cursor.getLong(3), cursor.getInt(4), cursor.getInt(5)));
+            } while (cursor.moveToNext());
+        }
+        if (cursor != null) {
+            cursor.close();
+        }
+        return albums;
+    }
 
     public static Album getAlbum(Context context, long id) {
         return getAlbum(makeAlbumCursor(context, "id=?", new String[]{String.valueOf(id)}));
--- app/src/main/java/com/young/timber/fragments/AlbumFragment.java
+++ app/src/main/java/com/young/timber/fragments/AlbumFragment.java
@@ -1,5 +1,7 @@
 package com.young.timber.fragments;
 
+import android.graphics.Rect;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,19 +10,100 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.young.timber.R;
+import com.young.timber.adapters.AlbumAdapter;
+import com.young.timber.dataloaders.AlbumLoader;
+import com.young.timber.utils.PreferencesUtility;
+import com.young.timber.widgets.BaseRecyclerView;
+import com.young.timber.widgets.DividerItemDecoration;
 
 public class AlbumFragment extends Fragment {
 
+    private AlbumAdapter adapter;
+    private BaseRecyclerView recyclerView;
+    private GridLayoutManager layoutManager;
+    private RecyclerView.ItemDecoration itemDecoration;
+    private boolean isGrid;
+    private PreferencesUtility mPreferences;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPreferences = PreferencesUtility.getInstance(getActivity());
+        isGrid = mPreferences.isAlbumInGrid();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recyclerview, container, false);
-
-
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setEmptyView(getActivity(), view.findViewById(R.id.list_empty), "No media found");
+        setLayoutManager();
+        if (getActivity() != null) {
+            new loadAlbums().execute("");
+        }
         return view;
     }
+
+    private void setLayoutManager() {
+        if (isGrid) {
+            layoutManager = new GridLayoutManager(getActivity(), 2);
+        } else {
+            layoutManager = new GridLayoutManager(getActivity(), 1);
+        }
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private class loadAlbums extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if (getActivity() != null) {
+                adapter = new AlbumAdapter(AlbumLoader.getAllAlbums(getActivity()), getActivity());
+            }
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            recyclerView.setAdapter(adapter);
+            if (getActivity() != null) {
+                setItemDecoration();
+            }
+
+        }
+    }
+
+    private void setItemDecoration() {
+        if (isGrid) {
+            int spacingInPixels = getActivity().getResources().getDimensionPixelSize(R.dimen.spacing_card_album_grid);
+            itemDecoration = new SpacesItemDecoration(spacingInPixels);
+        } else {
+            itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST);
+        }
+        recyclerView.addItemDecoration(itemDecoration);
+
+    }
+
+    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+        private int space;
+
+        public SpacesItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+            outRect.left = space;
+            outRect.top = space;
+            outRect.right = space;
+            outRect.bottom = space;
+        }
+    }
+
 }
